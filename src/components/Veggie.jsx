874cf4ea3476606e061ce7b0cb9f8311071dab11
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-05-18 15:17:42 +0530

    created category components and changed fonts of Popular and Veggie components

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -86,7 +86,7 @@ const Card = styled.div`
     width: 100%;
     text-align: center;
     font-weight: 600;
-    font-size: 1rem;
+    font-size: 0.8rem;
     height: 40%;
     display: flex;
     justify-content: center;
