f0321ba4a213dd6345f396fbc114549eadc2c165
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-05-18 14:45:34 +0530

    created and styled vegetarian picks part of home page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -12,47 +12,47 @@ function Popular() {
     getPopular();
   }, []);
 
-  const getPopular = async() => {
+  const getPopular = async () => {
 
     const check = localStorage.getItem('popular');
 
-    if(check) {
+    if (check) {
       setPopular(JSON.parse(check))
     } else {
       const api = await fetch(
         `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-        );
-        const data = await api.json();
-        localStorage.setItem('popular', JSON.stringify(data.recipes))
-        setPopular(data.recipes);
-        console.log(data.recipes)
+      );
+      const data = await api.json();
+      localStorage.setItem('popular', JSON.stringify(data.recipes))
+      setPopular(data.recipes);
+      console.log(data.recipes)
     }
   }
 
   return (
     <div>
-          <Wrapper>
-            <h3>Popular Picks</h3>
-            <Splide options={{
-              perPage: 4,
-              arrows: false,
-              pagination: false,
-              drag: "free",
-              gap: '2.5rem'
-            }}>
-            {popular.map((recipe) => {
-              return (
-                <SplideSlide key={recipe.id}>
-                  <Card>
-                    <p>{recipe.title}</p>
-                    <img src={recipe.image} alt={recipe.title} />
-                    <Gradient />
-                  </Card>
-                </SplideSlide>
-              );
-            })}
-            </Splide>
-          </Wrapper>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: "free",
+          gap: '2.5rem'
+        }}>
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </div>
   )
 }
