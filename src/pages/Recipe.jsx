b2a198fef3ed44d8dcfbacdc8050b272f4ce2652
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-06-01 15:22:14 +0530

    fixed bugs and api key

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -9,7 +9,7 @@ function Recipe() {
   const [activeTab, setActiveTab] = useState('instructions')
 
   const fetchDetails = async() => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.RECIPE_APP_API_KEY}`)
+    const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`)
     const detailData = await data.json();
     setDetails(detailData)
   }
